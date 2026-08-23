785d22b4ea6643105471711088fadb286b0314c6
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-23 14:48:31 +0530

    added footer navigaation with the required options

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { WaitlistModal } from "./components/WaitlistModal";
+
 import { Home } from "./pages/Home";
+import { PrivacyPolicy } from "./pages/PrivacyPolicy";
+import { TermsAndServices } from "./pages/TermsAndServices";
+import { RefundPolicy } from "./pages/RefundPolicy";
 
 export default function App() {
   const [isWaitlistOpen, setIsWaitlistOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState("home");
 
   const openWaitlist = () => {
     setIsWaitlistOpen(true);
@@ -16,15 +21,43 @@ export default function App() {
     setIsWaitlistOpen(false);
   };
 
+  const navigateTo = (page) => {
+    setCurrentPage(page);
+
+    // Scroll to top when opening a policy page
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "privacy":
+        return <PrivacyPolicy />;
+
+      case "terms":
+        return <TermsAndServices />;
+
+      case "refund":
+        return <RefundPolicy />;
+
+      default:
+        return <Home onJoinWaitlist={openWaitlist} />;
+    }
+  };
+
   return (
     <>
-      <Navbar onJoinWaitlist={openWaitlist} />
+      {currentPage === "home" && (
+        <Navbar onJoinWaitlist={openWaitlist} />
+      )}
 
       <main className="flex-grow z-10 relative">
-        <Home onJoinWaitlist={openWaitlist} />
+        {renderPage()}
       </main>
 
-      <Footer />
+      <Footer onNavigate={navigateTo} />
 
       <WaitlistModal
         isOpen={isWaitlistOpen}
